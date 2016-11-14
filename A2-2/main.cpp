246c6865b89d5b1366e9fd31d5a59cb502fa2fd6
Marcus Bätz <s59156@beuth-hochschule.de>
2016-11-14 13:41:23 +0100

    A2.2. angefangen

---
+++ A2-2/main.cpp
@@ -0,0 +1,30 @@
+// Testtreiber für Aufgabe 2: my::vector
+// Autor: Hartmut Schirmacher
+
+#include <iostream>
+#include "my_vector.h"
+#include "payload.h"
+
+using namespace std;
+
+void test_21();
+
+
+int main()
+{
+    cout << "Starting tests (2.1) --------------------" << endl;
+    //test_21();
+
+    // cout << "Starting tests (2.2) --------------------" << endl;
+    // test22();
+
+    cout << "End of tests ----------------------------" << endl;
+
+
+    my::vector<double> v;
+    v.reserve(1);
+    v.push_back(10.1);
+  //  v.at(1) =10.2;
+    std::cout <<v[0];
+    return 0;
+}
---
+++ A2-2/payload.h
@@ -0,0 +1,38 @@
+#ifndef PAYLOAD_H
+#define PAYLOAD_H
+
+#include <memory>
+#include <iostream>
+
+class Payload {
+
+public:
+    // data
+    float x, y, z;
+
+    // constructors
+    Payload(float xx=0, float yy=0, float zz=0) : x(xx), y(yy), z(zz) { count_++; }
+    Payload(const Payload& rhs) : x(rhs.x), y(rhs.y), z(rhs.z) { count_++; }
+
+    // destructor
+    ~Payload()  { count_--; }
+
+    // assignment - keeps count the same 
+    Payload& operator=(const Payload& rhs) { x=rhs.x; y=rhs.y; z=rhs.z; return *this; }
+
+    // comparison
+    bool operator==(const Payload& rhs) const {
+         return x==rhs.x && y==rhs.y && z==rhs.z; }
+
+
+
+    // tell count
+    static size_t count() { return count_; }
+
+private:
+    static size_t count_;
+
+};
+
+
+#endif // PAYLOAD_H
